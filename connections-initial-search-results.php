1e1e30fc74dc69e1bbb6b83f79f632d66b685c6b
Steven A. Zahm <shazahm1@hotmail.com>
2021-04-30 12:17:43 -0400

    Remove use of `create_function()`.

--- connections-initial-search-results.php
+++ connections-initial-search-results.php
@@ -362,10 +362,9 @@ if ( ! class_exists( 'Connections_Initial_Search_Results' ) ) {
 
 			add_action(
 				'admin_notices',
-				create_function(
-					'',
-					'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use this Connections addon.</p></div>\';'
-				)
+				function() {
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use this Connections addon.</p></div>';
+				}
 			);
 
 			return FALSE;
